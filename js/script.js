cad0bf84b74feecc35f47c0626e3c7cfac3b3218
Vladimir Rybalsky <alevoldon@gmail.com>
2020-09-07 20:41:54 +0300

    update files

--- js/script.js
+++ js/script.js
@@ -29,7 +29,27 @@ $(document).ready(function() {
 	} // end if
 
 
-
+    // OwlCarousel2
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
 
     
 }); 
